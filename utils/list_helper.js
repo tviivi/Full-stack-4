58d135fc98f096723445e0f08089ec362bd58c11
tviivi <viivi.tiihonen@helsinki.fi>
2018-02-19 11:29:45 +0200

    tehtävä 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
     return 1;
 }
 
+const totalLikes = (blogs) => {
+    var summa = 0;
+    blogs.forEach(blogi => {
+        summa = summa + blogi.likes
+    })
+    return summa;
+  }
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
