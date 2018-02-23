77cfbda41c35310f6f16f3487ee46543e40c83b1
tviivi <viivi.tiihonen@helsinki.fi>
2018-02-23 14:28:05 +0200

    tehtävä 4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -13,7 +13,7 @@ const formatBlog = (blog) => {
 }
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 } )
     response.json(blogs.map(Blog.format))
 })
 
--- controllers/users.js
+++ controllers/users.js
@@ -13,7 +13,7 @@ const formatUser = (user) => {
 }
 
 usersRouter.get('/', async (request, response) => {
-    const users = await User.find({})
+    const users = await User.find({}).populate('blogs', { title: 1, author: 1, url: 1, likes: 1, user: 1})
     response.json(users.map(User.format))
 })
 
